f0f7c02ad1af3f9ecd64e461ea8e4e442e6437e1
Logan <mlogan858@hotmail.com>
2021-04-16 00:03:45 -0400

    switch, & begining react-transition-groups

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Background.css';
 import MenuList from '../MenuList/MenuList';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import PortfolioPage from '../../pages/PortfolioPage/PortfolioPage'
 import HomePage from '../../pages/HomePage/HomePage'
 import ContactPage from '../../pages/ContactPage/ContactPage'
@@ -10,65 +10,66 @@ import PReactPage from '../../pages/PReactPage/PReactPage';
 import PSocketIOPage from '../../pages/PSocketIOPage/PSocketIOPage';
 import PPackDPage from '../../pages/PPackDPage/PPackDPage';
 import PLeafsletPage from '../../pages/PLeafsletPage/PLeafsletPage';
+import { TransitionGroup } from 'react-transition-group';
 
 class Background extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <MenuList 
-                    username={this.props.userName} 
-                    userEmail={this.props.userEmail} 
-                    theme={this.props.theme}
-                    color={this.props.color}
-                    accentColor={this.props.accentColor}
-                />
-                <Route path='/home' render={(props) => (
-                    <HomePage 
-                      {...props}
-                    />
-                )}/>
-                <Route path='/portfolio' render={(props) => (
-                    <PortfolioPage 
-                      portfolio={this.props.portfolio} 
-                      {...props}
-                    />
-                )}/>
-                <Route path='/contact' render={(props) => (
-                    <ContactPage 
-                      {...props}
-                    />
-                )}/>
-                <Route path='/about' render={(props) => (
-                    <AboutPage 
-                      {...props}
-                    />
-                )}/>
-
-
-
-                <Route path='/project-react-project' render={(props) => (
-                    <PReactPage 
-                    {...props}
-                    />
-                )}/>
-                <Route path='/project-socketio-project' render={(props) => (
-                    <PSocketIOPage 
-                      {...props}
-                    />
-                )}/>
-                <Route path='/project-packd' render={(props) => (
-                    <PPackDPage 
-                      {...props}
-                    />
-                )}/>
-                <Route path='/project-leafslet' render={(props) => (
-                    <PLeafsletPage 
-                      {...props}
-                    />
-                )}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+        <MenuList 
+            username={this.props.userName} 
+            userEmail={this.props.userEmail} 
+            theme={this.props.theme}
+            color={this.props.color}
+            accentColor={this.props.accentColor}
+        />
+        <Switch>
+          <Route path='/home' render={(props) => (
+              <HomePage 
+                {...props}
+              />
+          )}/>
+          <Route path='/portfolio' render={(props) => (
+              <PortfolioPage 
+                portfolio={this.props.portfolio} 
+                {...props}
+              />
+          )}/>
+          <Route path='/contact' render={(props) => (
+              <ContactPage 
+                {...props}
+              />
+          )}/>
+          <Route path='/about' render={(props) => (
+              <AboutPage 
+                {...props}
+              />
+          )}/>
+          <Route path='/project-react-project' render={(props) => (
+              <PReactPage 
+              {...props}
+              />
+          )}/>
+          <Route path='/project-socketio-project' render={(props) => (
+              <PSocketIOPage 
+                {...props}
+              />
+          )}/>
+          <Route path='/project-packd' render={(props) => (
+              <PPackDPage 
+                {...props}
+              />
+          )}/>
+          <Route path='/project-leafslet' render={(props) => (
+              <PLeafsletPage 
+                {...props}
+              />
+          )}/>
+          <Redirect to="/"/>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default Background;
